b4b5fab3d26f4195c83a6ad092c9931236171e63
Glitch (shocking-picturesque-tiger) <none>
2023-08-16 19:14:39 +0000

    Rewound to commit c4aca4455abbbf8c332ba5cb1f8a12c0d9ee9c3d + 0 edits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-const title = process.env.REACT_APP_TITLE
-
-
-
 
 function App() {
   return (
     <div className="App">
-    <h1>{title}</h1>
+    <h1>{process.env.REACT_APP_TITLE}</h1>
     <h3>{process.env.REACT_APP_DESCRIPTION}</h3>
     </div>
   );
