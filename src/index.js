3cc053722f7ee73de1e4c2343c301afc0d1c42ef
Glitch (starter-cra-and-express) <none>
2023-08-16 19:14:12 +0000

    🐷🍽 Checkpoint ./src/index.js:2703363/53 ./src/index.jsx:2703363/2 ./src/search.jsx:2703363/2088

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
+//import SearchPhotos from ".src/search.jsx";
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
